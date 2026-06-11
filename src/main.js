b7bf936593c43b83bde31fca5ca074bc41ba0935
Dimitrysaf <demetresmeliates+github@gmail.com>
2026-06-11 16:13:17 +0300

    .

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './styles.scss'
-import { initAll } from 'govuk-frontend'
-import { initLayout } from './components/layout.js'
-import { initHome } from './pages/home.js'
-
-initLayout()
-initHome()
-initAll()
